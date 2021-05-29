9c10e871ac35af9d48ec4686fb73ce4d73e0bd99
Andreco <320266241@alumno.enp.unam.mx>
2021-05-29 17:08:34 -0500

    Feat: inicio del canvas

---
+++ dynamics/Js/kikijump.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", ()=>{
+    var canvas = document.getElementById("pantalla");
+    var ctx = canvas.getContext("2d");
+    var pantalla = 0;
+
+    function fondo()
+    {
+        ctx.beginPath();
+            ctx.rect(0,0,canvas.width,canvas.height);
+            ctx.fillStyle = "#0D3A48";
+            ctx.fillStyle();
+        ctx.closePath();
+    }
+
+    function casillamenu(seleccion)
+    {
+        ctx.beginPath();
+            ctx.arc(canvas.width/2,canvas.height/2,25,0,Math.PI*2);
+            ctx.fillStyle = "#0D1434";
+            ctx.fill();
+            ctx.moveTo();
+        ctx.closePath();
+    }
+
+
+    function draw(){
+        if(pantalla == 0)
+        {
+            fondo();
+            casillamenu();
+        }
+    }
+
+    requestAnimationFrame(draw);
+    
+})
