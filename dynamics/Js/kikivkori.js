d0ce932471d42c86e39c1186a0b47256d7d8a63c
Andreco <320266241@alumno.enp.unam.mx>
2021-05-30 20:05:21 -0500

    FEAT: menu kikivkori

---
+++ dynamics/Js/kikivkori.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', ()=>{
+    let jugar = document.getElementById("play");
+    let controles = document.getElementById("controles");
+    let pantalla = document.getElementById("pantallaJuego")
+
+    jugar.addEventListener("click", ()=>{
+      /*el juego*/
+    })
+
+    controles.addEventListener("click", ()=>{
+        /*el juego*/
+      })
+
+
+})
