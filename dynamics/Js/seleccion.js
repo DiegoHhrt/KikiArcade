19299bec0c886c45318d92a976fd9051bbaa58a4
pato <patricio.ad26301@gmail.com>
2021-06-01 19:21:30 -0500

    FIgma

--- dynamics/Js/seleccion.js
+++ dynamics/Js/seleccion.js
@@ -1,13 +1,13 @@
-nombre =document.getElementById("nombre")
-color1 = document.getElementById("rojo")
-color2 = document.getElementById("azul")
-
-nombre.addEventListener("change", ()=>{
-    document.cookie='nombre='+nombre.value;
-})
-color1.addEventListener("click", ()=>{
-    document.cookie="pantalla=rojo"
-})
-color2.addEventListener("click", ()=>{
-    document.cookie='pantalla=azul';
-})
+nombre = document.getElementById("nombre");
+color1 = document.getElementById("rojo");
+color2 = document.getElementById("azul");
+//genera cookies de color y nombre
+nombre.addEventListener("change", () => {
+  document.cookie = "nombre=" + nombre.value;
+});
+color1.addEventListener("click", () => {
+  document.cookie = "pantalla=rojo";
+});
+color2.addEventListener("click", () => {
+  document.cookie = "pantalla=azul";
+});
